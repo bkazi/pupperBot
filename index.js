12fdba2b74852f099408f8ccfe4d2644af28f328
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-07-13 15:21:23 +0530

    Add functions to upload media to Twitter

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const {promisify} = require('util');
 const {URL} = require('url');
+const fs = require('fs');
+const readFile = promisify(fs.readFile);
+const stat = promisify(fs.stat);
 const request = require('request-promise-native');
-const Twitter = require('Twitter');
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 const GIPHY_BASE_URL = 'https://api.giphy.com';
 const GIPHY_SEARCH_PATH = 'v1/gifs/search';
@@ -31,4 +42,83 @@ async function getGIFS(searchQuery, limit=5) {
         console.error(e);
     }
     return data;
+}
+
+function isHttpPath(pathString) {
+    return /^https?:\/\//.test(pathString);
+}
+
+async function downloadMedia(path) {
+    const imgData = {
+        size: undefined,
+        image: undefined,
+        type: undefined,
+    };
+    try {
+        const data = await request.get(path, {encoding: null})
+            .on('response', (response) => {
+                imgData.size = response.headers['content-length'];
+                imgData.type = response.headers['content-type'];
+            });
+        imgData.image = data;
+    } catch (e) {
+        throw e;
+    }
+    return imgData;
+}
+
+async function uploadImage(client, path) {
+    try {
+        let data;
+        if (isHttpPath(path)) {
+            data = (await downloadMedia(path)).image;
+        } else {
+            data = await readFile(path);
+        }
+        const media = await client.post('media/upload', {media: data});
+        return media.media_id_string;
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function uploadGIF(client, path) {
+    try {
+        let size;
+        let data;
+        let type;
+        if (isHttpPath(path)) {
+            const imgData = await downloadMedia(path);
+            data = imgData.image;
+            size = imgData.size;
+            type = imgData.type;
+        } else {
+            data = await readFile(path);
+            size = (await stat(path)).size;
+            type = 'image/gif'; // lol this totally assumes it just a GIF
+        }
+
+        const mediaId = (await client.post('media/upload', {
+            command: 'INIT',
+            total_bytes: size,
+            media_type: type,
+        })).media_id_string;
+
+        await client.post('media/upload', {
+            command: 'APPEND',
+            media_id: mediaId,
+            media: data,
+            segment_index: 0,
+        });
+
+        await client.post('media/upload', {
+            command: 'FINALIZE',
+            media_id: mediaId,
+        });
+
+        return mediaId;
+    } catch (e) {
+        throw e;
+    }
+    return;
 }
